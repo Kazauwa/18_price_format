c6a1ff6885e4f8ffe8952e3a050dc00a8785fe82
Kazauwa <i.heart.d.dead@gmail.com>
2017-01-14 21:15:15 +0300

    add price formatting function & tests

--- format_price.py
+++ format_price.py
@@ -1,5 +1,41 @@
-def format_price(price):
-    pass
+import re
+from math import log10
+from argparse import ArgumentParser
+
+
+def prepare_input(price):
+    if isinstance(price, (int, float)):
+        return round(price)
+    if isinstance(price, str):
+        if price.isdigit():
+            return int(price)
+        if re.fullmatch('\d*[.,]?\d+', price):
+            price = price.replace(',', '.')
+            price = float(price)
+            return round(price)
+        raise ValueError('An input must match "[0-9].[0-9]" pattern!')
+    raise TypeError('An input must be int, float or string matching "[0-9].[0-9]" pattern!')
+
+
+def format_price(price, separation_grade=1000):
+    price = prepare_input(price)
+    formated_price = str(price)
+    insert_index = int(log10(separation_grade))
+    index_shift = insert_index + 1
+    price //= separation_grade
+    while price:
+        formated_price = '{} {}'.format(formated_price[:-insert_index], formated_price[-insert_index:])
+        insert_index += index_shift
+        price //= separation_grade
+    return formated_price
+
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--price', nargs='?', required=True, dest='price',
+                        help='Price to format')
+    parser.add_argument('-g', '--grade', type=int, nargs='?', default=1000,
+                        help='Separation grade. Must be a power of ten.')
+    options = parser.parse_args()
+
+    print(format_price(price=options.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,38 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class PriceFormattingTestCase(unittest.TestCase):
+    def testInteger(self):
+        price = 12345678
+        assert format_price(price) == '12 345 678'
+        assert format_price(price, separation_grade=1000000) == '12 345678'
+
+    def testFloat(self):
+        price = 1234.345
+        assert format_price(price) == '1 234'
+        price = 1234.567
+        assert format_price(price) == '1 235'
+
+    def testStr(self):
+        price = '12345.678'
+        assert format_price(price) == '12 346'
+        price = '12345,34'
+        assert format_price(price) == '12 345'
+
+    def testFailure(self):
+        with self.assertRaises(ValueError):
+            price = 'breakme'
+            format_price(price)
+            price = '123 45'
+            format_price(price)
+            price = str()
+            format_price(price)
+
+        with self.assertRaises(TypeError):
+            price = {'price': 1234}
+            format_price(price)
+
+
+if __name__ == '__main__':
+    unittest.main()
